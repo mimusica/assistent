5863921cb9c43d6a93ccf9197cf95bed2761ff62
Christophe Langenberg <christophe@langenberg.be>
2022-06-28 21:07:34 +0200

    [FEAT] our assistent listens to the print command

--- vca.py
+++ vca.py
@@ -1,6 +1,21 @@
+"""
+Dit script is een test met Vlaamse spraakherkenning.
+
+Indien men het woord "print" zegt gevolgd door een cijfer, dan wordt de zin n
+keer geprint.
+"""
 from vosk import Model, KaldiRecognizer
 import pyaudio
 import os
+import sys
+
+tekst_cijfers = {
+    "een": 1,
+    "twee": 2,
+    "drie": 3,
+    "vier": 4,
+    "vijf": 5
+}
 
 model = Model("/".join([os.getcwd(), "vosk-model-nl-spraakherkenning-0.6/"]))
 recognizer = KaldiRecognizer(model, 16000)
@@ -10,9 +25,17 @@ stream = mic.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, fr
 stream.start_stream()
 
 while True:
-    data = stream.read(4096)
-
-    if recognizer.AcceptWaveform(data):
-        text = recognizer.Result()
-        print(text[14:-3])
+    try:
+        data = stream.read(4096)
+    
+        if recognizer.AcceptWaveform(data):
+            text = recognizer.Result()
+            print(text[14:-3])
+            for k, v in tekst_cijfers.items():
+                if "print" in text and k in text:
+                    for i in range(v):
+                        print(text[14:-3])
+    except KeyboardInterrupt:
+        if input("Are you sure that you want to exit? [y/n] ").lower() == "y":
+            sys.exit()
 
