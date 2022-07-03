0d3457a6be7f32ce819172d7dde2c6549fcaa79d
Christophe Langenberg <christophe@langenberg.be>
2022-07-03 13:06:01 +0200

    [TEST] some more testing with pressing buttons, discovered how to prevent keystrokes to be printed on to our terminal

--- test.py
+++ test.py
@@ -1,14 +1,28 @@
 import keyboard
 import time
+import sys
+import os
 
 
 def main():
     while keyboard.is_pressed("a"):
+
         print("yeah baby! ")
-        time.sleep(1)
+        time.sleep(0.8)
 
 
 if __name__=='__main__':
+    # This prevents keyboard strokes to be printed on to the terminal.
+    os.system("stty -echo")
+    
     while True:
-        main()
+        try:
+            main()
+        except KeyboardInterrupt:
+            # re-enable keyboard strokes to be printed on to the terminal
+            os.system("stty echo")
+            if input("Are you sure that you want to quit? (y/n) ").lower() == "y":
+                sys.exit(0)
+            else:
+                os.system("stty -echo")
 
